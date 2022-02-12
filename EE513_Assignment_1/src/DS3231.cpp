5f17c1bbce4fa70b19321ae0b30578c29bba73c5
MegansCode27 <msmegw@gmail.com>
2022-02-12 17:47:39 +0000

    Code Changes adding temp

--- EE513_Assignment_1/src/DS3231.cpp
+++ EE513_Assignment_1/src/DS3231.cpp
@@ -13,6 +13,7 @@ using namespace std;
 #include<unistd.h> //On Unix-like systems, unistd.h is typically made up largely of system call wrapper functions such as fork, pipe and I/O primitives (read, write, close, etc.).
 #include<linux/i2c-dev.h> // bus interfaceioctl
 #include <Wire.h>
+#include <LiquidCrystal.h>
 #define BUFFER_SIZE 19  //allocates the memory for the OS  //0x00 to 0x12
 
 //states
