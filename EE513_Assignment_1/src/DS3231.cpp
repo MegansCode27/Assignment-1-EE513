897744f033007d827b2949495ca42a97b2134845
MegansCode27 <msmegw@gmail.com>
2022-02-14 20:29:31 +0000

    Code Changes adding setTime

--- EE513_Assignment_1/src/DS3231.cpp
+++ EE513_Assignment_1/src/DS3231.cpp
@@ -21,96 +21,89 @@ using namespace std;
 using namespace std;
 #define BUFFER_SIZE 19  //allocates the memory for the OS  //0x00 to 0x12
 
-class DS3231{
+class DS3231 {
 
 	//states
 	int file;
 
-	DS3231(){
-		 if((file=open("/dev/i2c-1", O_RDWR)) < 0){
-		      perror("failed to open the bus\n");
-		   }
-		   else {
-			   printf("Opened the bus\n");
-		   }
+public:
+	DS3231() {
+		if ((file = open("/dev/i2c-1", O_RDWR)) < 0) {
+			perror("failed to open the bus\n");
+		} else {
+			printf("Opened the bus\n");
+		}
 	}
 
-	~DS3231(){
+	~DS3231() {
 		close(file);
 	}
 
+	time_t GetTime(void) {
 
-    time_t GetTime(void){
-
-		union{
-		struct DS3231_time DS3231;
-		struct tm tm; }
-		tm;
-	    int ret= ioctl(file,I2C_SLAVE, &tm.DS3231);
-		if(ret<0){
-			throw std::system_error(errno,std::system_category(),"ioctl failed");
+		union {
+			//struct DS3231_time rtc;
+			struct tm tm;
+		} tm;
+		int ret = ioctl(file, I2C_SLAVE, &tm.rtc);
+		if (ret < 0) {
+			throw std::system_error(errno, std::system_category(),
+					"ioctl failed");
 
 		}
 		return mktime(&tm.tm);
 
+	}
 
 };
 
+// the time is in the registers in encoded decimal form
+int bcdToDec(char b) {
 
-    // the time is in the registers in encoded decimal form
-    int bcdToDec(char b) {
+	return (b / 16) * 10 + (b % 16);
 
-    	return (b/16)*10 + (b%16);
+}
+string display(uint8_t a) {
+	stringstream ss;
+	ss << setw(3) << (int) a << "(" << bitset<8>(a) << ")";
+	return ss.str();
+}
 
-    }
-    string display(uint8_t a) {
-       stringstream ss;
-       ss << setw(3) << (int)a << "(" << bitset<8>(a) << ")";
-       return ss.str();
-    }
+int main() {
 
+	int file;
+	// creates a integer value File
+	printf("Starting the DS3231 test application\n"); //messagae to the user
 
+	int addr = 0x68; // The Address to communicate
 
-int main(){
+	if (ioctl(file, I2C_SLAVE, addr) < 0) {
+		perror("Failed to connect to the sensor\n");
+		return 1;
+	} else {
+		printf("Connected to the DS3231 RTC Clock\n ");
+	}
 
-  int file;
-   // creates a integer value File
-   printf("Starting the DS3231 test application\n");//messagae to the user
+	char writeBuffer[1] = { 0x00 };
+	if (write(file, writeBuffer, 1) != 1) {
+		perror("Failed to reset the read address\n");
+		return 1;
+	}
 
+	char buf[BUFFER_SIZE];
+	if (read(file, buf, BUFFER_SIZE) != BUFFER_SIZE) {
+		perror("Failed to read in the buffer\n");
+		return 1;
+	}
+	printf("The RTC time is %02d:%02d:%02d\n", bcdToDec(buf[2]),
+			bcdToDec(buf[1]), bcdToDec(buf[0]));
 
-   int addr =0x68; // The Address to communicate
+	printf("The RTC date is %02d:%02d:%02d\n", bcdToDec(buf[4]),
+			bcdToDec(buf[5]), bcdToDec(buf[6]));
 
-   if(ioctl(file, I2C_SLAVE, addr) < 0){
-   perror("Failed to connect to the sensor\n");
-   return 1;
-   }
-   else
-   {
-	printf("Connected to the DS3231 RTC Clock\n ");
-   }
-
-   char writeBuffer[1] = {0x00};
-   if(write(file, writeBuffer, 1)!=1){
-      perror("Failed to reset the read address\n");
-      return 1;
-   }
-
-   char buf[BUFFER_SIZE];
-   if(read(file, buf, BUFFER_SIZE)!=BUFFER_SIZE){
-      perror("Failed to read in the buffer\n");
-      return 1;
-   }
-   printf("The RTC time is %02d:%02d:%02d\n", bcdToDec(buf[2]),
-    bcdToDec(buf[1]), bcdToDec(buf[0]));
-
-   printf("The RTC date is %02d:%02d:%02d\n", bcdToDec(buf[4]),
-      bcdToDec(buf[5]), bcdToDec(buf[6]));
-
-
-   //Temp
+	//Temp
 
 	int addrTemp = 0x11; // The Address to communicate
-
 
 	if (ioctl(file, I2C_SLAVE, addrTemp) < 0) {
 		perror("Failed to connect to the sensor\n");
@@ -120,13 +113,11 @@ int main(){
 		printf("Temperature is %2d degress \n", bcdToDec(buf[addrTemp]));
 	}
 
-   // set current date and time
-	 DS3231 rtc;
-	 time_t t= rtc.GetTime();
-	 std::cout<<"current Time is " <<ctime(&t)<<std::endl;
+	// set current date and time
+	DS3231 rtc;
+	time_t t = rtc.GetTime();
+	std::cout << "current Time is " << ctime(&t) << std::endl;
 
-   close(file);
-   return 0;
-}
-
+	close(file);
+	return 0;
 }
