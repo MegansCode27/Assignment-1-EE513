464931fba01862c5442efe50695872bdd92979ab
MegansCode27 <msmegw@gmail.com>
2022-02-16 00:07:58 +0000

    Code Changes adding new RTC time

--- EE513_Assignment_1/src/RTC.cpp
+++ EE513_Assignment_1/src/RTC.cpp
@@ -67,7 +67,7 @@ public:
     int addrTemp=0x11;
     int addrTempLow=0x12;
 
-	printf("Temperature is %2d:%2d degress \n",(buf[addrTemp]),(buf[addrTempLow]));
+	printf("Temperature is %02d:%02d degress \n",(buf[addrTemp]),(buf[addrTempLow]));
 		}
 
 
@@ -82,7 +82,7 @@ public:
 	       buf[0] = ltm->tm_sec;//Seconds
 	       buf[1] = ltm->tm_min; // Minutes
 	       buf[2] = 5+ltm->tm_hour; //Hours
-	       printf("The RTC current time is %02d:%02d:%03d\n", bcdToDec(buf[0]),
+	       printf("The RTC current time is %02d:%02d:%02d\n", bcdToDec(buf[0]),
 	       			bcdToDec(buf[1]), bcdToDec(buf[2]));
 
 
@@ -91,7 +91,7 @@ public:
 	       buf[5] = 1 + ltm->tm_mon; // Month
 	       buf[6] = 1900 + ltm->tm_year; // Year
 	      	       buf[2] = 5+ltm->tm_hour; //Hours
-	      	       printf("The RTC current Date is %02d:%02d:%03d\n", bcdToDec(buf[4]),
+	      	       printf("The RTC current Date is %02d:%02d:%02d\n", bcdToDec(buf[4]),
 	      	       			bcdToDec(buf[5]), bcdToDec(buf[6]));
 	      	       printf("Time amended on the RTC to current");
 	}
