dd58411662d0fdb0a15490166160303422a408d5
MegansCode27 <msmegw@gmail.com>
2022-02-16 10:10:40 +0000

    Code Changes adding new RTC Date

--- EE513_Assignment_1/src/RTC.cpp
+++ EE513_Assignment_1/src/RTC.cpp
@@ -66,10 +66,10 @@ public:
 
 	       buf[4] = ltm->tm_mday; //Day
 	       buf[5] = 1+ ltm->tm_mon; // Month
-	       buf[6] = ltm->tm_year; // Year
+	       buf[6] = 1900+ltm->tm_year; // Year
 
           cout <<1900+ltm->tm_year<<endl;
-	      printf("The RTC current Date is %02d: %02d: %d \n", buf[4],buf[5],buf[6]);
+	      printf("The RTC current Date is  %d-%02d-%02d\n", buf[4],buf[5],buf[6]);
 
 	}
 
