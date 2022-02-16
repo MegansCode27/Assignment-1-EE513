97f7d975f8a7c8063664772d58aef9060384b9ba
MegansCode27 <msmegw@gmail.com>
2022-02-16 09:26:32 +0000

    Code Changes adding new RTC time

--- EE513_Assignment_1/src/RTC.cpp
+++ EE513_Assignment_1/src/RTC.cpp
@@ -61,8 +61,8 @@ public:
 	       buf[2] = ltm->tm_hour; //Hours
 
 	       cout << ltm->tm_hour<<endl;
-	       printf("The RTC current time is %02d:%02d:%02d\n",  bcdToDec(buf[0]),
-	    		   bcdToDec(buf[1]),bcdToDec(buf[2]));
+	       printf("The RTC current time is %02d:%02d:%02d\n", buf[0],
+	    		   buf[1],buf[2]);
 
 
 
