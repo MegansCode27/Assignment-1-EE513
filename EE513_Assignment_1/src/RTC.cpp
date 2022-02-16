3b3906c1d76fc1d4815919c01f6ad49c21b9c73a
MegansCode27 <msmegw@gmail.com>
2022-02-16 09:11:06 +0000

    Code Changes adding new RTC time

--- EE513_Assignment_1/src/RTC.cpp
+++ EE513_Assignment_1/src/RTC.cpp
@@ -117,8 +117,8 @@ int main() {
 
     //DS3231 Times on startup
 
-	printf("The RTC time is %02d:%02d:%02d\n", rtc.bcdToDec(buf[2]),
-			rtc.bcdToDec(buf[1]), rtc.bcdToDec(buf[0]));
+	printf("The RTC time is %02d:%02d:%02d\n", rtc.bcdToDec(buf[0]),
+			rtc.bcdToDec(buf[1]), rtc.bcdToDec(buf[2]));
 
 	printf("The RTC date is %02d:%02d:%04d\n", rtc.bcdToDec(buf[4]),
 			rtc.bcdToDec(buf[5]), rtc.bcdToDec(buf[6]));
