af593123510dbf8c2e5ec0a1816b43477d93841d
MegansCode27 <msmegw@gmail.com>
2022-02-26 13:18:20 +0000

    Code Changes adding new RTC Alarm

--- EE513_Assignment_1/src/RTC.cpp
+++ EE513_Assignment_1/src/RTC.cpp
@@ -67,7 +67,6 @@ public:
 	       buf[5] = 1+ ltm->tm_mon; // Month
 	       buf[6] = (1900+ltm)->tm_year; // Year
 
-          cout <<1900+ ltm->tm_year<<endl;
 	      printf("The RTC current Date is  %d:%02d:%02d\n", buf[4],buf[5],buf[6]);
 
 	}
